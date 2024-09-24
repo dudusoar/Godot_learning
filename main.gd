40f426fd713fe9d9b7da198e70dfbe3496d540ae
YuChen Du <a1184657962@gmail.com>
2024-09-24 12:24:54 -0400

    update

--- main.gd
+++ main.gd
@@ -4,16 +4,20 @@ extends Node
 var score
 
 func _ready():
-	pass
-
-func game_over():
-	$ScoreTimer.stop()
-	$MobTimer.stop()
+	new_game()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
 
 
 func _on_start_timer_timeout() -> void:
@@ -22,6 +26,7 @@ func _on_start_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
